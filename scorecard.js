6f1769bee98a2a44b9b1ca61b644a42a9aa247a5
Brandon <93629403+hairforce1x@users.noreply.github.com>
2025-01-23 15:49:13 -0700

    Added files for different data categories.

--- scorecard.js
+++ scorecard.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const routes = require('./routes')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// routes
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
